8da4d6abe2fd30603e588bd426f90700d574437f
Nicosqui <nico50829@gmail.com>
2021-03-23 20:46:26 -0500

    first commit

---
+++ Sum_of_numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sum_of_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("The sum of the numbers from 1 to " + number + " is: " + SumIntegersFromOneToN(number));
+        }
+
+        static int SumIntegersFromOneToN(int n)
+        {
+            //base case:
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + SumIntegersFromOneToN(n - 1);
+            }
+        }
+    }
+}
